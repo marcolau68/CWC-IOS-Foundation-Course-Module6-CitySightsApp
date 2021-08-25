5f652b38c39cfdb5c493fb010d83000b068b9fde
Marco <marcolau68@gmail.com>
2021-08-25 11:58:26 +0800

    - add Constants which stores... constants

---
+++ City Sights App/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  City Sights App
+//
+//  Created by Marco Lau on 25/08/2021.
+//
+
+import Foundation
+
+struct Constants {
+    var apiKey = "9lZ0gZqYI6BsDYSDjXTAbbDLpG6pDiMw9y9KlwRLx9NQtKIm3v-BH9N9T7j3id-rU0pSdZ4Ea1hTjzyOY8mK3T8tc4Jt5mWFQaNs_fPx7bjNcswRlL5Tw9AiYK4lYXYx"
+    var yelpSearchURL = "https://api.yelp.com/v3/businesses/search"
+    var yelpSearchHTTPMethod = "GET"
+    
+
+}
+
+
+
+
+
