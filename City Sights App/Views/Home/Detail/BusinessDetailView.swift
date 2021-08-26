3d33a99bfaa87fc55ce55c38da580c2078aa75f1
Marco <marcolau68@gmail.com>
2021-08-26 16:29:45 +0800

    - finish BusinessDetailView - cannot use displayAddress for business, shows up as nil for some reason

--- City Sights App/Views/Home/Detail/BusinessDetailView.swift
+++ City Sights App/Views/Home/Detail/BusinessDetailView.swift
@@ -11,38 +11,44 @@ struct BusinessDetailView: View {
     var business: Business
     
     var body: some View {
-        let uiImage = UIImage(data: business.imageData ?? Data())
-        
-        VStack {
-            GeometryReader { geo in
-                Image(uiImage: uiImage ?? UIImage())
-                    .resizable()
-                    .frame(width: geo.size.width, height: geo.size.height)
-                    .clipped()
+        VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: 0) {
+                GeometryReader { geo in
+                    let uiImage = UIImage(data: business.imageData ?? Data())
+
+                    Image(uiImage: uiImage ?? UIImage())
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: geo.size.width, height: geo.size.height)
+                        .clipped()
+                }
+                .ignoresSafeArea(.all, edges: .top)
+                
+                ZStack(alignment: .leading)  {
+                    
+                    Rectangle()
+                        .frame(height: 36)
+                        .foregroundColor((business.isClosed ?? true) ? Color(.sRGB, red: 0.6, green: 0.6, blue: 0.6, opacity: 0.6):Color(.sRGB, red: 0, green: 0.3, blue: 0.5, opacity: 0.8))
+                    
+                    Text((business.isClosed ?? true) ? "Closed":"Open")
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                    
+                }
             }
             
-            ZStack(alignment: .leading)  {
-                
-                Rectangle()
-                    .frame(height: 36)
-                    .foregroundColor((business.isClosed ?? true) ? Color(.sRGB, red: 0.6, green: 0.6, blue: 0.6, opacity: 0.6):Color(.sRGB, red: 0, green: 0.3, blue: 0.5, opacity: 0.8))
-                
-                Text((business.isClosed ?? true) ? "Closed":"Open")
-                    .foregroundColor(.white)
-                    .padding()
-                
-            }
-            
-            VStack(alignment: .leading) {
+            VStack (alignment: .leading, spacing: 15) {
                 // Business name
                 Text(business.name ?? "")
                     .bold()
                     .font(.title)
                 
                 // Address and Yelp image
-                if business.location?.displayAddress != nil {
-                    ForEach(business.location!.displayAddress!, id: \.self) { line in
-                        Text(line)
+                if business.location != nil {
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("\(business.location!.address1!), \(business.location!.city!)")
+                            .font(.caption)
+                        Text("\(business.location!.state!), \(business.location!.country!)")
                             .font(.caption)
                     }
                 }
@@ -50,35 +56,71 @@ struct BusinessDetailView: View {
                 // Rating
                 Image("regular_\(business.rating ?? 0.0)")
                 
-                Divider()
-                
-                // Phone
-                HStack {
-                    HStack(spacing: 5) {
-                        Text("Phone: ")
-                            .bold()
-                        Text(business.phone ?? "")
-                    }
-                    
-                    Spacer()
-                    
-                    Text("Call")
-                }
-                .padding(.vertical)
-                
-                Divider()
-                
-                // Reviews
-                ContactRow(via: "Reviews", address: String(business.reviewCount ?? 0), keyword: "Read", url: business.url ?? "")
-                
-                // Website
-                ContactRow(via: "Website", address: business.url ?? "", keyword: "Visit", url: business.url ?? "")
-                    
             }
             .padding(.horizontal)
+            
+            Divider()
+            
+            // Phone
+            HStack {
+                Text("Phone: ")
+                    .bold()
+                Text(business.displayPhone ?? "")
+                
+                Spacer()
+                
+                Link("Call", destination: URL(string: "tel:\(business.phone ??  "")")!)
+            }
+            .padding()
+            
+            Divider()
+            
+            // Reviews
+            HStack {
+                Text("Reviews: ")
+                    .bold()
+                Text(String(business.reviewCount ?? 0))
+                
+                Spacer()
+                
+                Link("Read", destination: URL(string: business.url ?? "")!)
+            }
+            .padding()
+            
+            Divider()
+            
+            // Website
+            HStack {
+                Text("Website: ")
+                    .bold()
+                Text(String(business.url ?? ""))
+                    .lineLimit(1)
+                
+                Spacer()
+                
+                Link("Visit", destination: URL(string: business.url ?? "")!)
+            }
+            .padding()
+            
+            Divider()
+            
+            // Button
+            Button(action: {
+                
+            }, label: {
+                ZStack {
+                    Rectangle()
+                        .frame(height: 48)
+                        .foregroundColor(.blue)
+                        .cornerRadius(15)
+                    
+                    Text("Get Direction")
+                        .bold()
+                        .foregroundColor(.white)
+                }
+            })
+            .padding()
         }
-        .ignoresSafeArea(edges: .top)
-        
         
     }
 }
